1f97b4db2b556a82ef1beda0ff7388a4126d3c8d
bclin <bclin@192.168.2.101>
2021-01-09 15:03:45 -0500

    Moved Holes up
    set background from blade

--- Layer Heights.groovy
+++ Layer Heights.groovy
@@ -21,11 +21,13 @@ HashMap<String,List<Polygon>> polygonsByLayer = s.toPolygons()
 HashMap<String,ArrayList<CSG>> csgByLayers = s.extrudeLayers(10)
 // extrude just one layer to 10mm
 def holeParts = s.extrudeLayerToCSG(10,"1-Holes")
+.movez(3)
 // seperate holes and outsides using layers to differentiate
 def outsideParts = s.extrudeLayerToCSG(8,"2-Letters")
 					.difference(holeParts)
 // layers can be extruded at different depths					
 def boarderParts = s.extrudeLayerToCSG(6,"3-Flag")
 def BladeParts1 = s.extrudeLayerToCSG(4,"4-Blade")
+.difference(holeParts)
 def FlagParts1 = s.extrudeLayerToCSG(2,"3.5-FlagOutside")
 return [CSG.unionAll([boarderParts,outsideParts,BladeParts1,FlagParts1])]
